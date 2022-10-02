56c1d83ae8fca7cc335c5c377c09eb32fd957ddb
Lupitayoh <lupitallamasdev@gmail.com>
2022-10-01 19:39:24 -0500

    FE-1 Starting structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChronometerComponent } from './chronometer/chronometer.component';
+
+import { app_routing } from './app.routes';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+//import { AppRoutingModule } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChronometerComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   // AppRoutingModule,
+    app_routing
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
